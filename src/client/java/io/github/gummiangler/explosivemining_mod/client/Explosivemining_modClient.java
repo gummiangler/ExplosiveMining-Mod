ee63c423ce1be293532187d21f262a87f97328ed
NRF <nrf@localhost.localdomain>
2024-07-21 21:40:17 +0200

    Initial commit

---
+++ src/client/java/io/github/gummiangler/explosivemining_mod/client/Explosivemining_modClient.java
@@ -0,0 +1,10 @@
+package io.github.gummiangler.explosivemining_mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Explosivemining_modClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/gummiangler/explosivemining_mod/Explosivemining_mod.java
@@ -0,0 +1,10 @@
+package io.github.gummiangler.explosivemining_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Explosivemining_mod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
